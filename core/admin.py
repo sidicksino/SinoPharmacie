1127b3ffc9ae6cb78f3f6ba04899d1ee6be37303
SidickSino <sidickabdoulayesino1@gmail.com>
2026-01-20 09:15:14 +0200

    Create Superuser

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Pharmacy, Product
+
+@admin.register(Pharmacy)
+class PharmacyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'city', 'is_verified')
+    list_filter = ('city', 'is_verified')
+    search_fields = ('name', 'city')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'pharmacy', 'category', 'price', 'is_available')
+    list_filter = ('category', 'is_available', 'pharmacy__city')
+    search_fields = ('name', 'category', 'pharmacy__name')
